a9ccd5008ad4405230261f30529fa78e9c7528d8
Yam Alao <afk.puma@gmail.com>
2023-09-11 19:25:19 -0300

    update

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.urls import path
-from django.http import HttpResponse
+
 # As Views no Django são responsáveis por processar as requisições dos usuários, formar uma resposta e enviá-la de volta ao usuário.
 
 def home(request):

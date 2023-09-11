0d04a53e429b20544235d52608a8904bebb70dba
Yam Alao <afk.puma@gmail.com>
2023-09-11 04:42:46 -0300

    removido contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from recipes.views import home, contato, sobre
+from recipes.views import home
+
+#O código que você compartilhou é uma parte crucial de um projeto Django chamada 
+# URLconf (configuração de URL)1URLconf é um mapeamento entre padrões de caminho de URL para funções Python (suas views)2
 
 urlpatterns = [    
-    path('',home),
-    path('sobre/', sobre),
-    path('contato/', contato),
-    
+    path('',home),     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,10 @@
 from django.shortcuts import render
 from django.urls import path
 from django.http import HttpResponse
-# Create your views here.
+# As Views no Django são responsáveis por processar as requisições dos usuários, formar uma resposta e enviá-la de volta ao usuário.
 
 def home(request):
     return render(request, 'recipes/home.html', context={
         'name':"yam alao",
     })
 
-def contato(request):
-    return render(request,'recipes/contato.html')
-
-def sobre(request):
-    return HttpResponse('sobre')
